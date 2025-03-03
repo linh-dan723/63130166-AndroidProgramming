322b028c5399356f89a17292ca6f370623eb373c
Linh Đan <127948418+linh-dan723@users.noreply.github.com>
2025-03-03 08:16:49 +0700

    push

--- AppCong/app/src/main/java/tiil/edu/appcong/MainActivity.java
+++ AppCong/app/src/main/java/tiil/edu/appcong/MainActivity.java
@@ -14,11 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+            //gắn layout tương ứng
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    //đây là bộ lắng nghe và xửa lý sự kiện lick lên nut tính tông
+    public void XuLiCong(View view){
+
     }
 }
