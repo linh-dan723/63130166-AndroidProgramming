58b840ae0c86d35bd9a62081a99d5b871901667b
Linh Đan <127948418+linh-dan723@users.noreply.github.com>
2025-03-26 14:51:21 +0700

    error

--- testKT/app/src/main/java/com/example/testKT/MainActivity.java
+++ testKT/app/src/main/java/com/example/testKT/MainActivity.java
@@ -1,24 +1,40 @@
 package com.example.testKT;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnCau1 = findViewById(R.id.btnCau1);
+        Button btnCau2 = findViewById(R.id.btnCau2);
+        Button btnCau3 = findViewById(R.id.btnCau3);
+        Button btnCau4 = findViewById(R.id.btnCau4);
+
+        btnCau1.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, MainActivityCau1.class);
+            startActivity(intent);
+        });
+
+        btnCau2.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, MainActivityCau2.class);
+            startActivity(intent);
+        });
+
+        btnCau3.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, MainActivityCau3.class);
+            startActivity(intent);
+        });
+
+        btnCau4.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, ActivityCau4.class);
+            startActivity(intent);
         });
     }
 }
---
+++ testKT/app/src/main/java/com/example/testKT/MainActivityCau4.java
@@ -0,0 +1,12 @@
+package com.example.testKT;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityCau4 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_cau4);
+    }
+}
