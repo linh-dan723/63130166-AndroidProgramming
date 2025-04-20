15da865be16b16dbf1dd0dafb36a9415cafe0fba
Linh Đan <127948418+linh-dan723@users.noreply.github.com>
2025-04-20 20:05:58 +0700

    push

--- dynamicreplaycing/app/src/main/java/vn/edu/tinhoc123/dynamicreplaycing/Fragment1.java
+++ dynamicreplaycing/app/src/main/java/vn/edu/tinhoc123/dynamicreplaycing/Fragment1.java
@@ -1,26 +1,16 @@
 package vn.edu.tinhoc123.dynamicreplaycing;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment1#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Fragment1 extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +18,6 @@ public class Fragment1 extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment1.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Fragment1 newInstance(String param1, String param2) {
         Fragment1 fragment = new Fragment1();
         Bundle args = new Bundle();
@@ -58,7 +39,7 @@ public class Fragment1 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Đảm bảo file XML bạn dùng là fragment1.xml hoặc fragment_1.xml
         return inflater.inflate(R.layout.fragment_1, container, false);
     }
 }
--- dynamicreplaycing/app/src/main/java/vn/edu/tinhoc123/dynamicreplaycing/MainActivity.java
+++ dynamicreplaycing/app/src/main/java/vn/edu/tinhoc123/dynamicreplaycing/MainActivity.java
@@ -1,12 +1,12 @@
 package vn.edu.tinhoc123.dynamicreplaycing;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Thêm fragment vào container
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainerView_Content, new Fragment1())
+                .commit();
     }
 }
